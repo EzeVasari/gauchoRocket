1f12ff0c1ac51faa92ee2453cd7f1fe8d25001b7
EzeVasari <ezequielvasari@gmail.com>
2019-11-01 15:17:58 -0300

    Se cambiarion los directorios de js

--- gauchoRocket/Vista/reserva.php
+++ gauchoRocket/Vista/reserva.php
@@ -2,37 +2,6 @@
 <!DOCTYPE html>
 <html>
     <body>
-       
-<script type="text/javascript">
-    $(document).ready(function(){
-        var maxField = 5; // Numero maximo de campos
-        
-        var addButton = $('#btn-nuevaPersona'); // Selector del boton de Insertar
-        
-        var wrapper = $('#contenedor'); // Contenedor de campos
-        
-    
-        
-        var fieldHTML = '<h5>Persona</h5><div class="row"><div class="form-group col-md-6"><label>Nombre</label><input type="text" class="form-control" name="nombres[]" required></div><div class="form-group col-md-6"><label>Apellido</label><input type="text" class="form-control" name="apellidos[]" required></div><div class="form-group col-md-6"><label>DNI</label><input type="text" class="form-control" name="documentos[]" required></div><div class="form-group col-md-6"><label>Nick</label><input type="text" class="form-control" name="nicks[]" required></div></div>'; //Nuevos inputs 
-        
-        var x = 2; // Iniciamos el contador a 2
-        
-        $(addButton).click(function(){ // Una vez que se haga click en el boton
-            if(x <= maxField){ //Comprobamos el maximo
-                x++; //Incrementar
-                $(wrapper).append(fieldHTML); // Añadimos el HTML
-            }
-        });
-        
-        $(wrapper).on('click', '.remove_button', function(e){ // Una vez se ha hecho click en el boton de eliminar
-            e.preventDefault();
-            $(this).parent('div').remove(); //Eliminamos el div
-            x--; // Reducimos el contador en 1
-        });
-    });
-           
-</script>
-       
         <div class="container" style="margin-top: 5rem;">
            <h3 class="font-weight-bold" >Reserva</h3>
             <div class="row justify-content-between" id="tabla">
