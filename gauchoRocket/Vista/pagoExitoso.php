667cf333bf7e0452fd815d7c8dda7234670446e7
EzeVasari <ezequielvasari@gmail.com>
2019-11-11 15:18:49 -0300

    Se arreglo css de pagoExitoso.php

--- gauchoRocket/Vista/pagoExitoso.php
+++ gauchoRocket/Vista/pagoExitoso.php
@@ -31,31 +31,59 @@
  $precio = mysqli_query($conexion, $query);
  $datos = mysqli_fetch_assoc($precio);
 
-
+    
  $precioDeServicio = $datos["precioServicio"] * $datos["personas"];
  $precioDeCabina = $datos["precioCabina"] * $datos["personas"];
  $precioViaje = $datos["precioViaje"] * $datos["personas"];
  $precioTotal = $precioDeServicio+ $precioViaje + $precioDeCabina;
 
 
- echo "<br><br><br><br>
-
- <div class='col-0-md-7 text-center mb-3'>
-             <h2 class='font-weight-bold'>Su Pago Ha sido Registrado</h2>
-             <p class='text-muted'>Recuerde que una vez abonado, se habilitara su confirmacion de asistencia en el check-in</p>
-         </div>
- 				<div class='col mb-4'>
- 						<br>	
-                            <div class='card reservas text-center mx-auto'>
-                                <img src='".$datos['img']."' class='card-img-top' alt='...'>
-                                <div class='card-body'>
-                                    <h5 class='card-title'>".$datos['nombre']." (#".$codigoReserva.")</h5>
-                                    <p class='card-text'>".$datos['descripcion']."</p>
-                                    <h5 class='card-title'>Monto  Total abonado</h5>
-                                    <p class='card-text'>$".$precioTotal."</p>
-                                </div>
-                            </div>
-                        </div>         
-                        ";
+ echo '<br><div class="col-0-md-7 text-center mt-5">
+                <h2 class="font-weight-bold">Su pago ha sido registrado</h2>
+                <p class="text-muted">Recuerde que una vez abonado, se habilitara su confirmacion de asistencia en el check-in</p>
+            </div>
+            <div class="container mt-2">
+              <div class="row justify-content-center">
+                <div class="col-md-4 order-md-2 mb-2">
+                  <ul class="list-group mb-2">
+                    <li class="list-group-item d-flex justify-content-between lh-condensed">
+                      <div>
+                        <h6 class="my-0"><strong>'.$datos["nombre"].'</strong></h6>
+                        <small class="text-muted">'.$datos["descripcion"].'</small>
+                      </div>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between lh-condensed">
+                      <div>
+                        <h6 class="my-0">Vuelo <span class="text-muted">x'.$datos["personas"].'</span></h6>
+                        <small class="text-muted">Monto final de vuelo</small>
+                      </div>
+                      <span class="text-muted">$'.$datos["precioViaje"].'</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between lh-condensed">
+                      <div>
+                        <h6 class="my-0">Servicio <span class="text-muted">x'.$datos["personas"].'</span></h6>
+                        <small class="text-muted">Monto final del servicio </small>
+                      </div>
+                      <span class="text-muted">$'.$datos["precioServicio"].'</span>
+                    </li>
+                     <li class="list-group-item d-flex justify-content-between lh-condensed">
+                      <div>
+                        <h6 class="my-0">Cabina <span class="text-muted">x'.$datos["personas"].'</span></h6>
+                        <small class="text-muted">El monto de la cabina '.$datos["nombreCabina"].'</small>
+                      </div>
+                      <span class="text-muted">$'.$datos["precioCabina"].'</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between">
+                      <span>Total (USD)</span>
+                      <strong>$'.$precioTotal.'</strong>
+                    </li>
+                    <li class="list-group-item text-center">
+                      <i class="fas fa-check-circle fa-3x text-success"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>';
 
 ?>
+        
