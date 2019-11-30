7d826b9ac2eda0ab43a971b4a75a60a76c9320a4
osky6 <osc.eck11@gmail.com>
2019-11-29 23:02:29 -0300

    la validacion de ingreso de vuelo funciona correctamente agregando el destino, origen, fecha, matricula,tipo de viaje en la tabla viaje

--- gauchoRocket/Modelo/validarIngresoVuelo.php
+++ gauchoRocket/Modelo/validarIngresoVuelo.php
@@ -7,7 +7,7 @@ $nombreVuelo = $_POST["nombreVuelo"];
 $codigoVuelo= $_POST["codigoVuelo"];
 $descripcion = $_POST["descripcion"];
 $precio = $_POST["precio"];
-
+$fecha = $_POST["fecha"];
 
 
 $origen = $_POST["origen"];
@@ -60,20 +60,34 @@ $img =  $_FILES['img']['name'];
     
         $insert1 = mysqli_query($conexion, $query1);
 
-$query2 = "insert into viaje (codigoLugarOrigen)
-        values('".$origen."')
+$query2 = "UPDATE viaje SET codigoLugarOrigen='".$origen."'   
         where codigo='".$codigoVuelo."'";
     
         $insert2 = mysqli_query($conexion, $query2);
 
-$query3 = "insert into viaje (codigoLugarDestino)
-        values('".$destino."')
+$query3 = " UPDATE viaje SET codigoLugarDestino='".$destino."' 
         where codigo='".$codigoVuelo."'";
     
-        $insert3 = mysqli_query($conexion, $query3);        
+        $insert3 = mysqli_query($conexion, $query3);   
+
+        $query4 = " UPDATE viaje SET codigoTipoDeViaje='".$nivel."' 
+        where codigo='".$codigoVuelo."'";
+    
+        $insert4 = mysqli_query($conexion, $query4);   
+
+        $query5 = " UPDATE viaje SET matriculaEquipo='".$nave."' 
+        where codigo='".$codigoVuelo."'";
+    
+        $insert5 = mysqli_query($conexion, $query5);     
+
+        $query6 = " UPDATE viaje SET fecha='".$fecha."' 
+        where codigo='".$codigoVuelo."'";
+    
+        $insert6 = mysqli_query($conexion, $query6); 
 
 
-    if($insert1 == TRUE && $insert2=TRUE && $insert3){
+    if($insert1 == TRUE && $insert2==TRUE && $insert3==true && $insert4==true
+    && $insert5 == true &&  $insert6==true ){
         echo 'ingreso valido';
       echo  $directorio;
     }else {
